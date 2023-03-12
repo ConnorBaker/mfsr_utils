3542807659d9693ed7bf1baaac81e4d251139e81
Connor Baker <connorbaker01@gmail.com>
2023-03-12 01:35:26 -0500

    nix: bump

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/218265/head";
+    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/218044/head";
     nixGL = {
       url = "github:guibou/nixGL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hypothesis_torch_utils.url = "github:ConnorBaker/hypothesis_torch_utils";
+    hypothesis_torch_utils = {
+      url = "github:ConnorBaker/hypothesis_torch_utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   nixConfig = {
-    # Add the CUDA maintainer's cache and my cache to the binary cache list.
+    # Add the CUDA maintainer's cache
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cuda-maintainers.cachix.org"
@@ -50,37 +53,13 @@
           python3Packages = prev.python310Packages;
           cudaPackages = prev.cudaPackages_11_8;
         })
-        # Magma fix for CUDA 11.8
-        (final: prev: {
-          magma = prev.magma.overrideAttrs (oldAttrs: {
-            buildInputs = oldAttrs.buildInputs ++ [final.cudaPackages.cuda_profiler_api];
-          });
-        })
-        # Fix for OpenCV 4.5.4
-        (final: prev: {
-          opencv4 = prev.opencv4.override {
-            stdenv = prev.gcc11Stdenv;
-          };
-        })
-        # Fix for TorchVision not using the correct GCC
-        (final: prev: {
-          python3Packages = prev.python3Packages.overrideScope (python-final: python-prev: {
-            torchvision = python-prev.torchvision.overrideAttrs (oldAttrs: {
-              preConfigure = ''
-                export CC=${python-prev.torch.cudaPackages.cudatoolkit.cc}/bin/cc
-                export CXX=${python-prev.torch.cudaPackages.cudatoolkit.cc}/bin/c++
-                export CUDAHOSTCXX=${python-prev.torch.cudaPackages.cudatoolkit.cc}/bin/c++
-              '';
-            });
-          });
-        })
         overlay
         nixGL.overlays.default
       ];
     };
 
     inherit (pkgs) python3 python3Packages nixgl;
-    inherit (python3Packages) mfsr_utils;
+    inherit (python3Packages) mfsr_utils jupyter;
     inherit (nixgl.nvidiaPackages nvidiaDriver) nixGLNvidia;
   in {
     overlays.default = overlay;
@@ -88,6 +67,7 @@
       packages =
         [
           python3
+          jupyter
           mfsr_utils.propagatedBuildInputs
           nixGLNvidia
         ]
